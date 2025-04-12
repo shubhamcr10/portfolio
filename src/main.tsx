0d53d0963e1c9f969cacaa15ef664cf2409dc63d
ShubhamWSL <shubhamcr10@gmail.com>
2025-04-12 10:31:31 +0000

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-// createRoot(document.getElementById("root")!).render(<App />);
 const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
