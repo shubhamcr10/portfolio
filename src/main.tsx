9c47d1a8a24c7f57aa13b140fb5c6e9a5f3a1297
ShubhamWSL <shubhamcr10@gmail.com>
2025-04-12 16:44:02 +0000

    restore: trying to fix the routing issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <React.StrictMode>
-      <BrowserRouter basename="/portfolio">
-        <App />
-      </BrowserRouter>
+      <App />
     </React.StrictMode>
   );
 }
